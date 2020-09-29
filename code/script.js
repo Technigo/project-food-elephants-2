0964c31637b0e14ba4bc2d045dc6c7d7ee521986
Joel Harde <joelharde@Joels-MacBook-Pro.local>
2020-09-29 16:04:37 +0200

    Step 2 done

--- code/script.js
+++ code/script.js
@@ -10,16 +10,31 @@ fetch(apiUrl, { headers: { "user-key": apiKey } })
 .then(response => 
     response.json())
     .then(json => {
-        console.log(json);
-
         
-    console.log(json.restaurants[0].restaurant.name);
-    console.log(json.restaurants[0].restaurant.location.address);
-    console.log(json.restaurants[0].restaurant.average_cost_for_two);
-    console.log(json.restaurants[0].restaurant.currency);  
-    console.log(json.restaurants[0].restaurant.user_rating.aggregate_rating);
-    console.log(json.restaurants[0].restaurant.featured_image);  
+        let restaurants = json.restaurants 
+        let filteredRestaurants = restaurants.splice(0, 12)
+        //console.log(filteredRestaurants)
+
+        filteredRestaurants.forEach((restaurant) => {
+            let name = restaurant.restaurant.name;
+            let address = restaurant.restaurant.location.address;
+            const avCost = restaurant.restaurant.average_cost_for_two;
+            const currency = restaurant.restaurant.currency;
+            const rating = restaurant.restaurant.user_rating.aggregate_rating;
+            const img = restaurant.restaurant.featured_image;
+            console.log(`${name} : ${address}`);
+        });
+        
+        
+        
+    // console.log(json.restaurants[0].restaurant.name);
+    // console.log(json.restaurants[0].restaurant.location.address);
+    // console.log(json.restaurants[0].restaurant.average_cost_for_two);
+    // console.log(json.restaurants[0].restaurant.currency);  
+    // console.log(json.restaurants[0].restaurant.user_rating.aggregate_rating);
+    // console.log(json.restaurants[0].restaurant.featured_image);  
 
     
+
 
     });
