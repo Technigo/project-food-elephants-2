07f7ec4aef089564341050a7b1aab0bdf3f153ea
Nadia <63302901+Bluepoisson@users.noreply.github.com>
2020-09-30 10:03:37 +0200

    testingtesting

--- code/script.js
+++ code/script.js
@@ -2,31 +2,31 @@
 const apiKey = "a0b2ff4226123eea0f07250df8890683"
 const apiUrl = `https://developers.zomato.com/api/v2.1/search?entity_id=3&entity_type=city&cuisines=45`;
 const cityId = 3;
-const cuisineId = 45; 
+const cuisineId = 45;
 
 
 
 fetch(apiUrl, { headers: { "user-key": apiKey } })
-.then(response => 
+  .then(response =>
     response.json())
-    .then(json => {
-        
-        let restaurants = json.restaurants 
-        let filteredRestaurants = restaurants.splice(0, 12)
-        //console.log(filteredRestaurants)
+  .then(json => {
 
-        filteredRestaurants.forEach((restaurant) => {
-            const name = restaurant.restaurant.name;
-            const address = restaurant.restaurant.location.address;
-            const avCost = restaurant.restaurant.average_cost_for_two;
-            const currency = restaurant.restaurant.currency;
-            const rating = restaurant.restaurant.user_rating.aggregate_rating;
-            const img = restaurant.restaurant.featured_image;
-            
-            document.getElementById('restaurantContainer').innerHTML += `${name} ${address} ${rating}`
-            console.log(`${img} ${address} ${rating}`);
-        });
-  
+    let restaurants = json.restaurants
+    let filteredRestaurants = restaurants.splice(0, 12)
+    //console.log(filteredRestaurants)
+
+    filteredRestaurants.forEach((restaurant) => {
+      const name = restaurant.restaurant.name;
+      const address = restaurant.restaurant.location.address;
+      const avCost = restaurant.restaurant.average_cost_for_two;
+      const currency = restaurant.restaurant.currency;
+      const rating = restaurant.restaurant.user_rating.aggregate_rating;
+      const img = restaurant.restaurant.featured_image;
+
+      document.getElementById('restaurantContainer').innerHTML += `${name} ${address} ${rating}`
+      console.log(`${img} ${address} ${rating}`);
+    });
+
     // console.log(json.restaurants[0].restaurant.name);
     // console.log(json.restaurants[0].restaurant.location.address);
     // console.log(json.restaurants[0].restaurant.average_cost_for_two);
@@ -34,31 +34,31 @@ fetch(apiUrl, { headers: { "user-key": apiKey } })
     // console.log(json.restaurants[0].restaurant.user_rating.aggregate_rating);
     // console.log(json.restaurants[0].restaurant.featured_image);  
 
-     //sortera restaurangeRNA på pris : button? add event on clicK
+    //sortera restaurangeRNA på pris : button? add event on clicK
 
-//DATE FETCH RESTAURANTS SORTED BY PRICE RANGE IN ASCENDING ORDER
+    //DATE FETCH RESTAURANTS SORTED BY PRICE RANGE IN ASCENDING ORDER
 
-fetch(apiPriceAsc, { headers: { "user-key": apiKey } })
-.then(response =>
-  response.json())
-.then(json => {
-  let restaurants = json.restaurants
-  let filteredRestaurants = restaurants.splice(0, 12)
-  //console.log(filteredRestaurants)
-  filteredRestaurants.forEach((restaurant) => {
-    let name = restaurant.restaurant.name;
-    const avCost = restaurant.restaurant.average_cost_for_two;
-    let address = restaurant.restaurant.location.address;
-    const rating = restaurant.restaurant.user_rating.aggregate_rating;
+    fetch(apiPriceAsc, { headers: { "user-key": apiKey } })
+      .then(response =>
+        response.json())
+      .then(json => {
+        let restaurants = json.restaurants
+        let filteredRestaurants = restaurants.splice(0, 12)
+        //console.log(filteredRestaurants)
+        filteredRestaurants.forEach((restaurant) => {
+          let name = restaurant.restaurant.name;
+          const avCost = restaurant.restaurant.average_cost_for_two;
+          let address = restaurant.restaurant.location.address;
+          const rating = restaurant.restaurant.user_rating.aggregate_rating;
 
-    leRestaurantSection.innerHTML += `<li>${name}</li> <li>${avCost}</li> <li>${address}</li><li>${rating}</li>`
+          leRestaurantSection.innerHTML += `<li>${name}</li> <li>${avCost}</li> <li>${address}</li><li>${rating}</li>`
 
-    // leRestaurantSection.innerHTML += `<p>${name}</p> <p>${avCost}</p>`
+          leRestaurantSection.innerHTML += `<p>${name}</p> <p>${avCost}</p>`
+        });
+
+      });
+
+
+
+
   });
-
-});
-
-    
-
-
-    });
