b3f450d09be27c69bbb468c700f4a55f7b16f5c1
Nadia <63302901+Bluepoisson@users.noreply.github.com>
2020-09-30 13:18:47 +0200

    js btn and list  functions added

--- code/script.js
+++ code/script.js
@@ -4,7 +4,8 @@ const apiUrl = `https://developers.zomato.com/api/v2.1/search?entity_id=3&entity
 const cityId = 3;
 const cuisineId = 45;
 
-
+const apiPriceAsc = `https://developers.zomato.com/api/v2.1/search?entity_id=${3}&entity_type=city&cuisines=${45}&sort=cost&order=asc`
+const leRestaurantSection = document.getElementById("restaurantContainer")
 
 fetch(apiUrl, { headers: { "user-key": apiKey } })
   .then(response =>
@@ -23,8 +24,8 @@ fetch(apiUrl, { headers: { "user-key": apiKey } })
       const rating = restaurant.restaurant.user_rating.aggregate_rating;
       const img = restaurant.restaurant.featured_image;
 
-      document.getElementById('restaurantContainer').innerHTML += `${name} ${address} ${rating}`
-      console.log(`${img} ${address} ${rating}`);
+      document.getElementById('restaurantContainer').innerHTML += `<li>${name} ${rating} ${avCost} ${address}</li>`
+      // console.log(`${img} ${address} ${rating}`);
     });
 
     // console.log(json.restaurants[0].restaurant.name);
@@ -34,31 +35,72 @@ fetch(apiUrl, { headers: { "user-key": apiKey } })
     // console.log(json.restaurants[0].restaurant.user_rating.aggregate_rating);
     // console.log(json.restaurants[0].restaurant.featured_image);  
 
-     //sortera restaurangeRNA på pris : button? add event on clicK
 
-//DATE FETCH RESTAURANTS SORTED BY PRICE RANGE IN ASCENDING ORDER
 
-// fetch(apiPriceAsc, { headers: { "user-key": apiKey } })
-.then(response =>
-      response.json())
+    //DATE FETCH RESTAURANTS SORTED BY PRICE RANGE IN ASCENDING ORDER
+
+    fetch(apiPriceAsc, { headers: { "user-key": apiKey } })
+      .then(response =>
+        response.json())
       .then(json => {
-        let restaurants = json.restaurants
-        let filteredRestaurants = restaurants.splice(0, 12)
-        //console.log(filteredRestaurants)
-        filteredRestaurants.forEach((restaurant) => {
-          let name = restaurant.restaurant.name;
-          const avCost = restaurant.restaurant.average_cost_for_two;
-          let address = restaurant.restaurant.location.address;
-          const rating = restaurant.restaurant.user_rating.aggregate_rating;
+        document.getElementById("btnPrice").addEventListener("click", () => {
+          let restaurants = json.restaurants
+          let filteredRestaurants = restaurants.splice(0, 12)
 
-          leRestaurantSection.innerHTML += `<li>${name}</li> <li>${avCost}</li> <li>${address}</li><li>${rating}</li>`
+          //FUNCTION TO CLEAR PREVIOUS CONTENT
+          leRestaurantSection.innerHTML = "";
 
-          // leRestaurantSection.innerHTML += `<p>${name}</p> <p>${avCost}</p>`
+          filteredRestaurants.forEach((restaurant) => {
+            let name = restaurant.restaurant.name;
+            const avCost = restaurant.restaurant.average_cost_for_two;
+            let address = restaurant.restaurant.location.address;
+            const rating = restaurant.restaurant.user_rating.aggregate_rating;
+
+            //PRINTS SORTED DATA LIST BY ASCENDING PRICE RANGE
+            leRestaurantSection.innerHTML += `<li>${name}</li> <li>Rating: ${rating}</li><li>${avCost}</li>`;
+          });
+
+
+
         });
 
+
+
+
+      });
+
+  });
+
+
+//FUNCTION THAT CREATES THE BUTTON
+
+function sortPriceBtn() {
+  const makeBtn = document.createElement("button");
+  document.body.appendChild(makeBtn);
+};
+
+fetch(apiPriceAsc, { headers: { "user-key": apiKey } })
+  .then(response =>
+    response.json())
+  .then(json => {
+    document.getElementById("btnRating").addEventListener("click", () => {
+      let restaurants = json.restaurants
+      let filteredRestaurants = restaurants.splice(0, 12)
+
+      //FUNCTION TO CLEAR PREVIOUS CONTENT
+      leRestaurantSection.innerHTML = "";
+
+      filteredRestaurants.forEach((restaurant) => {
+        let name = restaurant.restaurant.name;
+        const avCost = restaurant.restaurant.average_cost_for_two;
+        let address = restaurant.restaurant.location.address;
+        const rating = restaurant.restaurant.user_rating.aggregate_rating;
+
+        //PRINTS SORTED DATA LIST BY ASCENDING PRICE RANGE
+        leRestaurantSection.innerHTML += `<li>${name}</li> <li>Rating: ${rating}</li>`;
       });
 
 
 
-
+    });
   });
